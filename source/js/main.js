5fe3b1d95c5c83dca740b31b3d39f9760a17fe4d
Rostyslav Miniukov <thresh1337@gmail.com>
2020-09-20 21:05:12 +0300

    Підключив функції до головного скріпту

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+(function () {
+  // Активація першочергових функцій
+  var DOMLoadedHandler = function () {
+    window.sections.activate();
+    window.hamburger.activate();
+    window.select.handle();
+    window.modal.activate();
+  };
+
+  document.addEventListener('DOMContentLoaded', DOMLoadedHandler);
+})();
