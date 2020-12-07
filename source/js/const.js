5cc3491e668d4fbf0c9f9c08dce4857c1d5e2a7b
Rostyslav Miniukov <thresh1337@gmail.com>
2020-12-07 16:28:43 +0200

    Створюємо рендер-методи. WIP

---
+++ source/js/const.js
@@ -0,0 +1,26 @@
+export const SHAKE_ANIMATION_TIMEOUT = 600;
+
+export const KeyCode = {
+  ENTER: 13,
+  ESC: 27
+};
+
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  AFTEREND: `afterend`,
+  BEFOREEND: `beforeend`,
+  BEFOREBEGIN: `beforebegin`,
+};
+
+export const SECTION = {
+  INTRO: `intro`,
+  DATA: `income-data`,
+  RESULTS: `results`,
+};
+
+export const ErrorMessage = {
+  VALUE_MISSING: `Введіть дані`,
+  RANGE_OVERFLOW: `Значення не може бути більше `,
+  RANGE_UNDERFLOW: `Значення не може бути менше `,
+  SELECT_OPTION: `Виберіть тип ГПА`
+};
