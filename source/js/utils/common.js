9a31e4d941840f5af5b131570692d28fb39f7b20
Rostyslav Miniukov <thresh1337@gmail.com>
2020-12-07 16:31:54 +0200

    Створюємо допоміжні-утилітарні методи. WIP

---
+++ source/js/utils/common.js
@@ -0,0 +1,26 @@
+import {KeyCode} from '../const.js';
+
+// Блокуємо прокрутку сторінки
+export const blockBodyScroll = () => {
+  document.body.style.overflowY = `hidden`;
+};
+
+// Розблокуємо прокрутку сторінки
+export const unblockBodyScroll = () => {
+  document.body.style.overflowY = `auto`;
+};
+
+// Перевірка на натиснення кнопки ESC
+export const isEscKey = (evt) => {
+  return evt.keyCode === KeyCode.ESC;
+};
+
+// Виділяємо поле вводу
+export const highlightInput = (input) => {
+  input.classList.add(`data__input--error`);
+};
+
+// Знімаємо виділення поля вводу
+export const resetHighlightInput = (input) => {
+  input.classList.remove(`data__input--error`);
+};
